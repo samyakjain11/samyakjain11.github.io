c40c6d975c8fd866751a954f9a828f95d79f7286
samyak <samyak257@gmail.com>
2022-09-12 13:38:15 -0500

    update

---
+++ src/pages/Home.js
@@ -0,0 +1,49 @@
+import React from "react";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import EmailIcon from "@material-ui/icons/Email";
+import GithubIcon from "@material-ui/icons/GitHub";
+//import IconButton from "@mui/material/IconButton"
+import "../styles/Home.css";
+
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="about">
+        <h2> Hi! I am Samyak Jain.</h2>
+        <div className="prompt">
+          <p>I am currently a junior studying Computer Science and Math at UIUC.</p>
+          <LinkedInIcon src="https://www.linkedin.com/feed"/> 
+          <EmailIcon/>
+          <GithubIcon/>
+          
+        </div>
+      </div>
+      <div className="skills">
+        <h1> Skills</h1>
+        <ol className="list">
+          <li className="item">
+            <h2> Front-End</h2>
+            <span>
+              ReactJS, Angular, Redux, HTML, CSS, React Native, Flutter, NPM,
+              Ionic, BootStrap, MaterialUI, Yarn, TailwindCSS, StyledComponents
+            </span>
+          </li>
+          <li className="item">
+            <h2>Back-End</h2>
+            <span>
+              NodeJS, Java Spring, .NET, ExpressJS, GraphQL, ApolloServer,
+              MySQL, MongoDB, DynamoDB, DigitalOcean, AWS S3, MS SQL
+            </span>
+          </li>
+          <li className="item">
+            <h2>Languages</h2>
+            <span>JavaScript, Java, Python, C#, C, C++, TypeScript, Go</span>
+          </li>
+        </ol>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
