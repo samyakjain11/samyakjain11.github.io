6b11bdf3a1161e51a3064417fb90373ba22b8f69
samyak <samyak257@gmail.com>
2023-12-16 13:33:18 -0600

    pull from minport

---
+++ src/lib/constants.ts
@@ -0,0 +1,13 @@
+export const LINKS = {
+  github: "https://github.com/samyakjain11",
+  linkedin: "https://www.linkedin.com/in/samyakjain11/",
+  medium: "https://ladvace.medium.com/",
+  discord: "https://discordapp.com/users/163300027618295808",
+  resume: "https://drive.google.com/file/d/1nvNyNR1YKWBwOFkoNl_XsqdScLq3cnHo/view?usp=sharing",
+};
+
+export const loaderAnimation = [
+  ".loader",
+  { opacity: [1, 0], pointerEvents: "none" },
+  { easing: "ease-out" },
+];
