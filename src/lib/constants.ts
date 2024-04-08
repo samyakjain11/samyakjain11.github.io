1cd6ecb1538148b3cc12ce237a78cef6805fbaee
Samyak Jain <70160731+samyakjain11@users.noreply.github.com>
2024-04-08 16:32:28 -0500

    Update constants.ts
    changed resume

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,7 +3,7 @@ export const LINKS = {
   linkedin: "https://www.linkedin.com/in/samyakjain11/",
   medium: "https://ladvace.medium.com/",
   discord: "https://discordapp.com/users/163300027618295808",
-  resume: "https://drive.google.com/file/d/1nvNyNR1YKWBwOFkoNl_XsqdScLq3cnHo/view?usp=sharing",
+  resume: "https://drive.google.com/file/d/1abS6gTQ6hQY63F9pfJqnLUO-P4CqbHYL/view?usp=sharing",
 };
 
 export const loaderAnimation = [
